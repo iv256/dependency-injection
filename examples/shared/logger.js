fd3a979fdfd250ce82c885e63d82de90cdc6ea7f
iv256 <armen.iv.256@gmail.com>
2026-05-12 18:54:47 +0400

    examples: add shared logger

---
+++ examples/shared/logger.js
@@ -0,0 +1,13 @@
+export class Logger {
+  debug(message) {
+    console.debug(`[DEBUG] ${message}`);
+  }
+
+  warn(message) {
+    console.warn(`[WARN] ${message}`);
+  }
+
+  error(message) {
+    console.error(`[ERROR] ${message}`);
+  }
+}
