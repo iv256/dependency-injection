5e950c53e80f4ae6279a405a2d8a5c74d86ba227
iv256 <armen.iv.256@gmail.com>
2026-05-12 19:16:56 +0400

    examples: add class injection script

---
+++ examples/class.injection.js
@@ -0,0 +1,55 @@
+import { createContainer } from '../src/index.js';
+import {
+  AppConfig,
+  Logger,
+  ApiClient,
+  UserService,
+  UserView,
+} from './shared/index.js';
+
+console.log('Example: class injection');
+
+const di = createContainer();
+
+// Register injector extension.
+di.useInjector();
+
+UserView.diKeyMap = {
+  user: 'user',
+};
+
+di.define('app.config', () => {
+  return AppConfig;
+});
+
+di.define('logger', () => {
+  return new Logger();
+});
+
+di.define('api.client', [
+  'app.config',
+  'logger',
+], (config, logger) => {
+  return new ApiClient({
+    config,
+    logger,
+  });
+});
+
+di.define('user.service', [
+  'api.client',
+], (apiClient) => {
+  return new UserService({
+    apiClient,
+  });
+});
+
+di.define('user', [
+  'user.service',
+], async (userService) => {
+  return userService.getUser(1);
+});
+
+const userView = await di.createClassInstance(UserView);
+
+console.log(userView.render());
