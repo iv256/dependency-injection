3b49e14fcdab7226bab0a963446aa557539ffcd0
iv256 <armen.iv.256@gmail.com>
2026-05-12 19:08:42 +0400

    examples: add shared exports

---
+++ examples/shared/index.js
@@ -0,0 +1,13 @@
+export { AppConfig } from './app.config.js';
+export { Logger } from './logger.js';
+export { ApiClient } from './api.client.js';
+
+export { User } from './user.js';
+export { Photo } from './photo.js';
+
+export { UserService } from './user.service.js';
+export { PhotoService } from './photo.service.js';
+
+export { UserView } from './user.view.js';
+export { PhotoView } from './photo.view.js';
+export { ProfilePageView } from './profile-page.view.js';
